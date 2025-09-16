58171b2019caf6bf476bf8fb4e57ba2b8a7b3a18
cellarstella <laura.cellarstella@gmail.com>
2025-09-16 14:25:34 -0700

    Updates

---
+++ .next/server/chunks/[root-of-the-server]__f547b805._.js
@@ -0,0 +1,81 @@
+module.exports = [
+"[project]/.next-internal/server/app/favicon.ico/route/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__, module, exports) => {
+
+}),
+"[externals]/next/dist/compiled/next-server/app-route-turbo.runtime.dev.js [external] (next/dist/compiled/next-server/app-route-turbo.runtime.dev.js, cjs)", ((__turbopack_context__, module, exports) => {
+
+const mod = __turbopack_context__.x("next/dist/compiled/next-server/app-route-turbo.runtime.dev.js", () => require("next/dist/compiled/next-server/app-route-turbo.runtime.dev.js"));
+
+module.exports = mod;
+}),
+"[externals]/next/dist/compiled/@opentelemetry/api [external] (next/dist/compiled/@opentelemetry/api, cjs)", ((__turbopack_context__, module, exports) => {
+
+const mod = __turbopack_context__.x("next/dist/compiled/@opentelemetry/api", () => require("next/dist/compiled/@opentelemetry/api"));
+
+module.exports = mod;
+}),
+"[externals]/next/dist/compiled/next-server/app-page-turbo.runtime.dev.js [external] (next/dist/compiled/next-server/app-page-turbo.runtime.dev.js, cjs)", ((__turbopack_context__, module, exports) => {
+
+const mod = __turbopack_context__.x("next/dist/compiled/next-server/app-page-turbo.runtime.dev.js", () => require("next/dist/compiled/next-server/app-page-turbo.runtime.dev.js"));
+
+module.exports = mod;
+}),
+"[externals]/next/dist/server/app-render/work-unit-async-storage.external.js [external] (next/dist/server/app-render/work-unit-async-storage.external.js, cjs)", ((__turbopack_context__, module, exports) => {
+
+const mod = __turbopack_context__.x("next/dist/server/app-render/work-unit-async-storage.external.js", () => require("next/dist/server/app-render/work-unit-async-storage.external.js"));
+
+module.exports = mod;
+}),
+"[externals]/next/dist/server/app-render/work-async-storage.external.js [external] (next/dist/server/app-render/work-async-storage.external.js, cjs)", ((__turbopack_context__, module, exports) => {
+
+const mod = __turbopack_context__.x("next/dist/server/app-render/work-async-storage.external.js", () => require("next/dist/server/app-render/work-async-storage.external.js"));
+
+module.exports = mod;
+}),
+"[externals]/next/dist/shared/lib/no-fallback-error.external.js [external] (next/dist/shared/lib/no-fallback-error.external.js, cjs)", ((__turbopack_context__, module, exports) => {
+
+const mod = __turbopack_context__.x("next/dist/shared/lib/no-fallback-error.external.js", () => require("next/dist/shared/lib/no-fallback-error.external.js"));
+
+module.exports = mod;
+}),
+"[externals]/next/dist/server/app-render/after-task-async-storage.external.js [external] (next/dist/server/app-render/after-task-async-storage.external.js, cjs)", ((__turbopack_context__, module, exports) => {
+
+const mod = __turbopack_context__.x("next/dist/server/app-render/after-task-async-storage.external.js", () => require("next/dist/server/app-render/after-task-async-storage.external.js"));
+
+module.exports = mod;
+}),
+"[externals]/next/dist/server/app-render/action-async-storage.external.js [external] (next/dist/server/app-render/action-async-storage.external.js, cjs)", ((__turbopack_context__, module, exports) => {
+
+const mod = __turbopack_context__.x("next/dist/server/app-render/action-async-storage.external.js", () => require("next/dist/server/app-render/action-async-storage.external.js"));
+
+module.exports = mod;
+}),
+"[project]/src/app/favicon--route-entry.js [app-rsc] (ecmascript)", ((__turbopack_context__) => {
+"use strict";
+
+__turbopack_context__.s([
+    "GET",
+    ()=>GET,
+    "dynamic",
+    ()=>dynamic
+]);
+var __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$server$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__ = __turbopack_context__.i("[project]/node_modules/next/server.js [app-rsc] (ecmascript)");
+;
+const contentType = "image/x-icon";
+const cacheControl = "public, max-age=0, must-revalidate";
+const buffer = Buffer.from("AAABAAEAICAAAAEAIACoEAAAFgAAACgAAAAgAAAAQAAAAAEAIAAAAAAAABAAACMuAAAjLgAAAAAAAAAAAAAgHyP/IB8j/yAfI/8gHyP/IB8j/yAfI/8gHyP/IB8j/yAfI/8gHyP/IB8j/yAfI/8gHyP/IB8j/yAfI/8gHyP/IB8j/yAfI/8gHyP/IB8j/yAfI/8gHyP/IB8j/yAfI/8gHyP/IB8j/yAfI/8gHyP/IB8j/yAfI/8gHyP/IB8j/yAfI/8gHyP/IB8j/yAfI/8gHyP/IB8j/yAfI/8gHyP/IB8j/yAfI/8gHyP/IB8j/yAfI/8gHyP/IB8j/yAfI/8gHyP/IB8j/yAfI/8gHyP/IB8j/yAfI/8gHyP/IB8j/yAfI/8gHyP/IB8j/yAfI/8gHyP/IB8j/yAfI/8gHyP/IB8j/yAfI/8gHyP/IB8j/yAfI/8gHyP/IB8j/yAfI/8gHyP/IB8j/yAfI/8gHyP/IB8j/yAfI/8gHyP/IB8j/yAfI/8gHyP/IB8j/yAfI/8gHyP/IB8j/yAfI/8gHyP/IB8j/yAfI/8gHyP/IB8j/yAfI/8gHyP/IB8j/yAfI/8gHyP/IB8j/yAfI/8gHyP/IB8j/yAfI/8fHiL/IB8j/yEgJP8iISX/IiEl/yIhJf8iISX/IiEl/yIhJf8iISX/IiEl/yIhJf8iISX/IiEl/yIhJf8iISX/IiEl/yAfI/8gHyP/Hx4i/yAfI/8gHyP/IB8j/yAfI/8gHyP/IB8j/yAfI/8gHyP/IB8j/yAfI/8gHyP/Hx4i/zAvM/8hICX/X15h/6yrrf+rqqz/q6qs/6uqrP+rqqz/q6qs/6uqrP+rqqz/q6qs/6uqrP+rqqz/q6qs/6yrrf+WlZj/Ly4y/yIhJf8uLTH/Hx4i/yAfI/8gHyP/IB8j/yAfI/8gHyP/IB8j/yAfI/8gHyP/Hx4i/yIhJP9paGv/srKz/zEwNP99fX//8vLy/+/v7//v7+//7+/v/+/v7//v7+//7+/v/+/v7//v7+//7+/v/+/v7//v7+//8fHy/9HR0v81NDj/MjE1/66ur/9lZGf/IB8k/x8eIv8gHyP/IB8j/yAfI/8gHyP/IB8j/x4dIf8sKy//jIuN//Dw8P/t7e3/R0ZK/zAvM/9FREj/UVBT/1ZVWP9WVVj/VlVY/1ZVWP9WVVj/VlVY/1ZVWP9WVVj/VlVY/1ZVWP9JSUz/QUBD/yMiJv9GRkn/7ezt/+7u7/+Hhoj/Kikt/x4dIf8gHyP/IB8j/yAfI/8dHCD/Pz5C/7Kxs//7+/v///////n5+f9kY2b/MzI0/zIxM/+op6n/2tra/9fX1//X19f/1tbW/9fX1//X19f/19fX/9fX1//X19f/0tLT/1pZXP88Ojz/LCsu/2NhZf/5+fn///////r6+v+tra7/PDs//x0cIf8gHyP/IiEl/1xbXv/S0tP//////////////////////4SDhf8xMDL/MjEz/1pZXP9ubnD/a2pt/21rbP+CeWj/bWxt/2tqbf9ram3/a2pt/2tqbf9ra23/Ojo9/0NBQ/8qKSz/g4KE///////////////////////Pzs//V1ZZ/yAfI/9wcHL/6enp////////////////////////////p6ao/y4sL/9JSEj/MjAy/0VESP9+foD/f3+A/5mFU/+BfHL/fn6B/35+gP9+foD/fHx+/z8+Qf83Njj/TEpK/yYlKP+mpaf////////////////////////////m5ub/bGtu/5+foP/////////////////////////////////Fxcb/MTAz/1VTUv9APj//Z2Zp/8bGxv/CwsT/1bt3/9TFnf/BwsT/wsLC/8PDxP/Fxcb/XFte/0VDRP9LSUr/Kyou/8XExv////////////////////////////////+fn6D/n5+h/////////////////////////////////+Dg4f88Oz//TEpK/0lHR/8vLTH/MTA0/1NUWf+VfDz/nn0p/1lZXf9cXF7/Q0NG/y8uMf8yMTP/TEpK/0A/QP84OD3/3tvS///88P///////////////////////////6Cgof+goKH/////////////////////////////////8fHy/1JRVP9BQED/XFpY/05NTP9CQUT/ysvO/+bTnv/suzr/497U/+Tk5f+Ghoj/NzU3/1lXVv9aWFf/NTQ3/11RNv/v2qL///zz////////////////////////////oaGi/6Ggov/////////////////////////////////9/f3/b25x/zc2N/9cWln/UU9P/ysqLf8+PkT/alo2/92jCv90Yzv/Q0NJ/zMyNv88Ojz/W1lZ/1tYVf90XSL/x6JA//324f////////////////////////////////+ioqT/oaGj//////////////////////////////////////+RkJL/MC4x/1pYV/9QT07/JCMn/xwcIf8/NBz/3KAD/4pnD/8dHCH/Hx4i/z8+Qf9oYVL/rIgo/7qKCf+zqIv//////////////////////////////////////6Sjpf+ioqP////////////////////////+/P///vr//////7Kztv8rKjD/VVRU/01MTP8jISX/Hx4j/zQtIP/TmwX/w5AH/y4oIf8dHSP/Xk8t/8iYGf/UnAb/U0Me/7CwtP//////////////////////////////////////pKSm/6OipP////////////////////////z2//ztxv/746P/1L6D/1pKJ/9iWUb/TkpF/yEgJP8cHCT/KiYh/8KPCP/nqAH/W0gZ/2pSF//WnAX/5akI/3NeKf8uLTL/z8/Q//////////////////////////////////////+mpaf/pKOl//////////////////////////////35//3y1v/x04T/x5ca/9GcDP/BkRL/l3EP/3ZaFP9gSxj/wI4I//GvAP/QmAX/5acC/+qrBP+Wezb/Ozo7/0BAQ//n5+f//////////////////////////////////////6emqP+kpKb///////////////////////////////////////f39v9uZ1r/dl4g/8CRFP/lpwH/8rAA/+mqAf/rqwH/764A//GvAP/trQD/l3Ub/1hWVP8wLzP/Wlpc//b29v//////////////////////////////////////qKiq/5STlf/+/v7//////////////////////////////////////3p5ff8wLzT/Qj45/1NCGf+Zcw7/26ED//CvAP/vrgD/764A/+ysAP+8jQ7/inI1/zkzJv96en3///////////////////////////////////////7///+Yl5n/UFBT/+jo6P//////////////////////////////////////nZ2f/ysqLf8yMTT/Gxsj/zQsIP++jAj/8rAA//CvAP/vrgD/7KwB//KwAP/urQH/x5IF/8ysWf/98tb///78////////////////////////////6urq/1RTVv8lJCj/ra2u//////////////////////////////////////+9vb7/LCsu/yYmKv8pJSH/nHUO//CvAP/gpAP/46YC/++uAP+Ycg//d1wW/6N9Gf+7iwz/6bIf//O+Mf/20W//++q+//778f////////////////+xsLL/JiUp/x0bIP9iYWT/9vb2/////////////////////////////////9ra2v80Mzj/IyEi/4lnEf/trQH/w5AH/1lHGf+gdw7/87AA/3xfFP8dHif/MC4x/zo3Nf/a1MP//vDM//njpv/43ZL/+eOn//303f////7/9/f4/2VkZ/8dGyD/Hx4i/y4tMf/IyMn/////////////////////////////////7e3t/0tKTf90WBL/4aQC/6B3Df86MR//Gxsk/2NNF//trQD/kGwQ/x8fJP8iISX/SklM/+zs7f///////////////////fn//vz1///////Ly8z/Ly4y/x8eIv8gHyP/Hh0h/359gP/+/v7////////////////////////////5+fn/pY9X/8WQA/94XBT/KCQi/x4eI/8fHiP/Ny4f/9GZBf+jeQ3/ISAj/x0cIP9lZWf/+vr7/////////////////////////////v7+/4GBg/8eHSH/IB8j/yAfI/8eHSH/PTxA/97e3/////////////////////////////3tw//KoTT/UkEZ/x8fI/8fHyP/IB8j/yAfI/8hICP/nnYO/7SFCv8mIyL/HRwg/4eHif/////////////////////////////////g4OH/Pz5C/x4dIf8gHyP/IB8j/yAfI/8gHyP/m5qc////////////////////////9+H/+d+c/5aOfP8eHiT/IB8j/yAfI/8gHyP/IB8j/x0dI/9kThj/vIoJ/ysnIf8fHiP/mZia/////////////////////////////////56eoP8hICT/IB8j/yAfI/8gHyP/IB8j/x0cIP9RUVT/7u7u////////////9PTy/8K6p/92c2//NjU6/yAfI/8gHyP/IB8j/yAfI/8gHyP/Hx4j/zcvH/+rfwv/Ni4f/x8eI/85OTz/fn2A/8nJyv/39/f////////////w8PD/VFNW/x0cIP8gHyP/IB8j/yAfI/8gHyP/Hx4i/ycmKv+2trf/7Ozs/6ysrf9fXmH/LCsv/x0cIf8fHiL/IB8j/yAfI/8gHyP/IB8j/yAfI/8gHyP/IiAj/4JjE/9CNx3/Hh4j/x8eIv8eHSH/Li4x/2ZmaP+zs7T/8PDw/7q5u/8oJyv/Hx4i/yAfI/8gHyP/IB8j/yAfI/8gHyP/Hx4i/0hHSv9MS07/IyIm/x0cIP8fHiL/IB8j/yAfI/8gHyP/IB8j/yAfI/8gHyP/IB8j/yAfI/8eHSP/UkIb/0U4Hf8eHSP/IB8j/yAfI/8fHiL/HRwg/yUkKP9QT1P/S0pN/x8eIv8gHyP/IB8j/yAfI/8gHyP/IB8j/yAfI/8gHyP/Hh0h/x4dIf8gHyP/IB8j/yAfI/8gHyP/IB8j/yAfI/8gHyP/IB8j/yAfI/8gHyP/IB8j/x8eI/8uKSH/NC0g/x8eI/8gHyP/IB8j/yAfI/8gHyP/IB8j/x4dIf8eHSH/IB8j/yAfI/8gHyP/IB8j/yAfI/8gHyP/IB8j/yAfI/8gHyP/IB8j/yAfI/8gHyP/IB8j/yAfI/8gHyP/IB8j/yAfI/8gHyP/IB8j/yAfI/8gHyP/IB8j/yAfI/8iICP/IB8j/yAfI/8gHyP/IB8j/yAfI/8gHyP/IB8j/yAfI/8gHyP/IB8j/yAfI/8gHyP/AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=", 'base64');
+if ("TURBOPACK compile-time falsy", 0) //TURBOPACK unreachable
+;
+function GET() {
+    return new __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$server$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["NextResponse"](buffer, {
+        headers: {
+            'Content-Type': contentType,
+            'Cache-Control': cacheControl
+        }
+    });
+}
+const dynamic = 'force-static';
+}),
+];
+
+//# sourceMappingURL=%5Broot-of-the-server%5D__f547b805._.js.map
---
+++ .next/server/chunks/ssr/[root-of-the-server]__b89f583b._.js
@@ -0,0 +1,230 @@
+module.exports = [
+"[project]/.next-internal/server/app/page/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__, module, exports) => {
+
+}),
+"[project]/src/app/favicon.ico.mjs { IMAGE => \"[project]/src/app/favicon.ico (static in ecmascript)\" } [app-rsc] (structured image object, ecmascript, Next.js Server Component)", ((__turbopack_context__) => {
+
+__turbopack_context__.n(__turbopack_context__.i("[project]/src/app/favicon.ico.mjs { IMAGE => \"[project]/src/app/favicon.ico (static in ecmascript)\" } [app-rsc] (structured image object, ecmascript)"));
+}),
+"[project]/src/app/layout.tsx [app-rsc] (ecmascript, Next.js Server Component)", ((__turbopack_context__) => {
+
+__turbopack_context__.n(__turbopack_context__.i("[project]/src/app/layout.tsx [app-rsc] (ecmascript)"));
+}),
+"[project]/src/app/component/SocialImage.tsx [app-rsc] (ecmascript)", ((__turbopack_context__) => {
+"use strict";
+
+__turbopack_context__.s([
+    "default",
+    ()=>SocialImage
+]);
+var __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__ = __turbopack_context__.i("[project]/node_modules/next/dist/server/route-modules/app-page/vendored/rsc/react-jsx-dev-runtime.js [app-rsc] (ecmascript)");
+;
+function SocialImage({ image, link, alt }) {
+    return /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["jsxDEV"])("a", {
+        href: link,
+        target: "_blank",
+        children: /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["jsxDEV"])("img", {
+            className: "size-8",
+            src: image,
+            alt: alt
+        }, void 0, false, {
+            fileName: "[project]/src/app/component/SocialImage.tsx",
+            lineNumber: 10,
+            columnNumber: 13
+        }, this)
+    }, void 0, false, {
+        fileName: "[project]/src/app/component/SocialImage.tsx",
+        lineNumber: 9,
+        columnNumber: 9
+    }, this);
+}
+}),
+"[project]/src/app/page.tsx [app-rsc] (ecmascript)", ((__turbopack_context__) => {
+"use strict";
+
+__turbopack_context__.s([
+    "default",
+    ()=>Home
+]);
+var __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__ = __turbopack_context__.i("[project]/node_modules/next/dist/server/route-modules/app-page/vendored/rsc/react-jsx-dev-runtime.js [app-rsc] (ecmascript)");
+var __TURBOPACK__imported__module__$5b$project$5d2f$src$2f$app$2f$component$2f$SocialImage$2e$tsx__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__ = __turbopack_context__.i("[project]/src/app/component/SocialImage.tsx [app-rsc] (ecmascript)");
+;
+;
+function Home() {
+    return /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["jsxDEV"])("div", {
+        className: "font-sans",
+        children: [
+            /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["jsxDEV"])("header", {
+                className: "flex flex-col align-end linen-header pt-8 pl-8 pr-8 h-60",
+                children: [
+                    /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["jsxDEV"])("div", {
+                        className: "flex gap-4 mt-2 min-w-32 justify-end",
+                        children: [
+                            /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["jsxDEV"])(__TURBOPACK__imported__module__$5b$project$5d2f$src$2f$app$2f$component$2f$SocialImage$2e$tsx__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["default"], {
+                                image: "/LinkedIn_icon.svg",
+                                link: "https://www.linkedin.com/in/laura-pelofske-26a69775/",
+                                alt: "LinkedIn profile"
+                            }, void 0, false, {
+                                fileName: "[project]/src/app/page.tsx",
+                                lineNumber: 8,
+                                columnNumber: 11
+                            }, this),
+                            /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["jsxDEV"])(__TURBOPACK__imported__module__$5b$project$5d2f$src$2f$app$2f$component$2f$SocialImage$2e$tsx__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["default"], {
+                                image: "/github-mark.svg",
+                                link: "https://github.com/cellarstella",
+                                alt: "GitHub profile"
+                            }, void 0, false, {
+                                fileName: "[project]/src/app/page.tsx",
+                                lineNumber: 13,
+                                columnNumber: 11
+                            }, this),
+                            /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["jsxDEV"])(__TURBOPACK__imported__module__$5b$project$5d2f$src$2f$app$2f$component$2f$SocialImage$2e$tsx__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["default"], {
+                                image: "/file.svg",
+                                link: "./files/LPelofske_resume.pdf",
+                                alt: "PDF resume"
+                            }, void 0, false, {
+                                fileName: "[project]/src/app/page.tsx",
+                                lineNumber: 18,
+                                columnNumber: 11
+                            }, this)
+                        ]
+                    }, void 0, true, {
+                        fileName: "[project]/src/app/page.tsx",
+                        lineNumber: 7,
+                        columnNumber: 9
+                    }, this),
+                    /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["jsxDEV"])("div", {
+                        className: "flex flex-row gap-2 justify-between",
+                        children: /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["jsxDEV"])("h1", {
+                            className: "text-4xl md:text-6xl font-bold text-white relative top-2.5",
+                            children: "Laura Pelofske: Senior Software Developer"
+                        }, void 0, false, {
+                            fileName: "[project]/src/app/page.tsx",
+                            lineNumber: 25,
+                            columnNumber: 11
+                        }, this)
+                    }, void 0, false, {
+                        fileName: "[project]/src/app/page.tsx",
+                        lineNumber: 23,
+                        columnNumber: 15
+                    }, this)
+                ]
+            }, void 0, true, {
+                fileName: "[project]/src/app/page.tsx",
+                lineNumber: 6,
+                columnNumber: 7
+            }, this),
+            /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["jsxDEV"])("main", {
+                className: "flex flex-col gap-[32px] row-start-2 p-8 pb-20 items-center sm:items-start",
+                children: [
+                    /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["jsxDEV"])("section", {
+                        children: /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["jsxDEV"])("p", {
+                            className: "text-lg md:text-xl mb-6",
+                            children: "I am a versatile and highly adaptable software development contributor, adept in utilizing ES6 JavaScript, Typescript, Testing Library, React, and diverse CSS methodologies to surpass user acceptance criteria. My expertise truly stands out when seamlessly translating visual designs to any framework. With years of experience, I swiftly identify potential UI pitfalls and consistently provide alternative solutions for integrating new interactions into complex applications across various devices. Recognized as a dependable resource and mentor in UI styling, I prioritize documenting and commenting on my work for the benefit of others."
+                        }, void 0, false, {
+                            fileName: "[project]/src/app/page.tsx",
+                            lineNumber: 36,
+                            columnNumber: 11
+                        }, this)
+                    }, void 0, false, {
+                        fileName: "[project]/src/app/page.tsx",
+                        lineNumber: 35,
+                        columnNumber: 9
+                    }, this),
+                    /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["jsxDEV"])("h2", {
+                        children: "Skills"
+                    }, void 0, false, {
+                        fileName: "[project]/src/app/page.tsx",
+                        lineNumber: 41,
+                        columnNumber: 9
+                    }, this),
+                    /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["jsxDEV"])("ul", {
+                        className: "list-disc",
+                        children: [
+                            /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["jsxDEV"])("li", {
+                                children: "Next.js, React, TypeScript, Node.js"
+                            }, void 0, false, {
+                                fileName: "[project]/src/app/page.tsx",
+                                lineNumber: 43,
+                                columnNumber: 11
+                            }, this),
+                            /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["jsxDEV"])("li", {
+                                children: "LESS, SCSS, Tailwind, CSS3 and beyond"
+                            }, void 0, false, {
+                                fileName: "[project]/src/app/page.tsx",
+                                lineNumber: 44,
+                                columnNumber: 11
+                            }, this),
+                            /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["jsxDEV"])("li", {
+                                children: "Github, Agile, JIRA, Trello"
+                            }, void 0, false, {
+                                fileName: "[project]/src/app/page.tsx",
+                                lineNumber: 45,
+                                columnNumber: 11
+                            }, this),
+                            /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["jsxDEV"])("li", {
+                                children: "GraphQL, SQL, Python, some Java"
+                            }, void 0, false, {
+                                fileName: "[project]/src/app/page.tsx",
+                                lineNumber: 46,
+                                columnNumber: 11
+                            }, this),
+                            /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["jsxDEV"])("li", {
+                                children: "Written communication, User Experience, Accessibility (WCAG 2.2)"
+                            }, void 0, false, {
+                                fileName: "[project]/src/app/page.tsx",
+                                lineNumber: 47,
+                                columnNumber: 11
+                            }, this),
+                            /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["jsxDEV"])("li", {
+                                children: "SCRUM leadership, Roadmapping, cross functional communication and problem solving"
+                            }, void 0, false, {
+                                fileName: "[project]/src/app/page.tsx",
+                                lineNumber: 48,
+                                columnNumber: 11
+                            }, this)
+                        ]
+                    }, void 0, true, {
+                        fileName: "[project]/src/app/page.tsx",
+                        lineNumber: 42,
+                        columnNumber: 9
+                    }, this)
+                ]
+            }, void 0, true, {
+                fileName: "[project]/src/app/page.tsx",
+                lineNumber: 33,
+                columnNumber: 7
+            }, this),
+            /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["jsxDEV"])("footer", {
+                className: "py-6 text-center text-sm text-gray-500",
+                children: [
+                    "© ",
+                    new Date().getFullYear(),
+                    " Laura Pelofske. All rights reserved."
+                ]
+            }, void 0, true, {
+                fileName: "[project]/src/app/page.tsx",
+                lineNumber: 55,
+                columnNumber: 7
+            }, this)
+        ]
+    }, void 0, true, {
+        fileName: "[project]/src/app/page.tsx",
+        lineNumber: 5,
+        columnNumber: 5
+    }, this);
+}
+}),
+"[project]/src/app/page.tsx [app-rsc] (ecmascript, Next.js Server Component)", ((__turbopack_context__) => {
+
+__turbopack_context__.n(__turbopack_context__.i("[project]/src/app/page.tsx [app-rsc] (ecmascript)"));
+}),
+"[externals]/next/dist/shared/lib/no-fallback-error.external.js [external] (next/dist/shared/lib/no-fallback-error.external.js, cjs)", ((__turbopack_context__, module, exports) => {
+
+const mod = __turbopack_context__.x("next/dist/shared/lib/no-fallback-error.external.js", () => require("next/dist/shared/lib/no-fallback-error.external.js"));
+
+module.exports = mod;
+}),
+];
+
+//# sourceMappingURL=%5Broot-of-the-server%5D__b89f583b._.js.map
